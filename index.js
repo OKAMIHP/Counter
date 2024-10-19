ac1b40c4e9a1260ba90203bf2341252135cdce48
OKAMIHP <haddrain.polanco.7@gmail.com>
2024-10-18 21:29:21 -0400

    Goodbye again

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked (log it out)
 // change the count-el in the HTML to reflect the new count
-// use console.log() to log the number of clicks - then test that in the browser’s console Hi
+// use console.log() to log the number of clicks - then test that in the browser’s console
 let count = 0;
 function increment() {
   count = count + 1;
   document.getElementById("count-el").innerText = count;
   console.log(count);
 }
+
 function save() {
   let save1 = 0;
   save1 = count;
@@ -17,16 +18,12 @@ function save() {
 }
 
 function autoIncrement() {
-  let x = prompt("What value?");
-  for (let i = 0; i < x; i++) {
-    count = count + 1;
-    document.getElementById("count-el").innerText = count;
-  }
-  console.log(count);
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
 }
 
-function increment() {
-  let x = prompt("What value?");
+function SuperIncrement() {
+  let x = Math.floor(Math.random() * 50);
   for (let i = 0; i < x; i++) {
     setTimeout(autoIncrement, 1000);
   }
