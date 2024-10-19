288aad9043238e6817c4ca7666be79ea6a073e08
OKAMIHP <haddrain.polanco.7@gmail.com>
2024-10-18 21:27:24 -0400

    Hello

--- index.js
+++ index.js
@@ -3,33 +3,32 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked (log it out)
 // change the count-el in the HTML to reflect the new count
-// use console.log() to log the number of clicks - then test that in the browser’s console
-let count = 0
-function increment(){
-    count = count + 1
-    document.getElementById("count-el").innerText=count
-    console.log(count)
+// use console.log() to log the number of clicks - then test that in the browser’s console Hi
+let count = 0;
+function increment() {
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
+  console.log(count);
 }
 
-
-function save(){
-    let save1 = 0
-    save1 = count
-    document.getElementById("save2").innerText = save2.innerText + save1 + "-";
+function save() {
+  let save1 = 0;
+  save1 = count;
+  document.getElementById("save2").innerText = save2.innerText + save1 + "-";
 }
 
-function autoIncrement(){
-    let x = prompt("What value?")
-    for(let i = 0; i < x; i++){
-        count = count + 1
-        document.getElementById("count-el").innerText=count
-    }
-    console.log(count)
+function autoIncrement() {
+  let x = prompt("What value?");
+  for (let i = 0; i < x; i++) {
+    count = count + 1;
+    document.getElementById("count-el").innerText = count;
+  }
+  console.log(count);
 }
 
-function increment(){
-    let x = prompt("What value?")
-    for(let i = 0; i < x; i++){
-    setTimeout(autoIncrement, 1000)
-}
+function increment() {
+  let x = prompt("What value?");
+  for (let i = 0; i < x; i++) {
+    setTimeout(autoIncrement, 1000);
+  }
 }
